90e9c38e725a542f5d9324c46b6b6555c1a3bbbe
Chauhan Vivek Singh <105410640+chauhanviveksingh@users.noreply.github.com>
2024-09-10 02:11:42 +0530

    Install react js tailwind css and create the first template

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+
+export default function App() {
+  // eslint-disable-next-line react/no-unknown-property
+  return <h1 clssName='text-red-500'>App</h1>;
+}
