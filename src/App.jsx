3bb2bcebfba9ca84bec1dad2a559ca986df1be52
Chauhan Vivek Singh <105410640+chauhanviveksingh@users.noreply.github.com>
2024-09-10 08:43:48 +0530

    Create pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
+/* eslint-disable react/jsx-no-undef */
 // eslint-disable-next-line no-unused-vars
-import React from 'react';
-
+import { BrowserRouter,Routes, Route  } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Profile from './pages/Profile';
+import Signin from './pages/Signin';
+import SignOut from './pages/SignOut';
 export default function App() {
+  return <BrowserRouter>
+  <Routes>
+  <Route path="/" element={<Home/>}/>
+  <Route path="/about" element={<About/>}/>
+  <Route path="/profile" element={<Profile/>}/>
+  <Route path="/sign-in" element={<Signin/>}/>
+  <Route path="/sign-up" element={<SignOut/>}/>
+  
+  </Routes>
+ 
+  
+  
+  </BrowserRouter>;
   // eslint-disable-next-line react/no-unknown-property
-  return <h1 clssName='text-red-500'>App</h1>;
 }
